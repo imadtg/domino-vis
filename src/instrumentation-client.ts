4afea569e0619e2a09bccfeda8f4a0b3f449f4c3
imadtg <i.haidi@outlook.com>
2025-06-30 22:10:07 +0100

    Check if posthog key is set

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,5 +1,7 @@
 import posthog from "posthog-js";
-
+if(typeof process.env.NEXT_PUBLIC_POSTHOG_KEY == 'undefined'){
+  throw new Error('Posthog key env variable is not set!')
+}
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: "2025-05-24",
