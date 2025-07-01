eb99b1f3e1a39f3f773fbfb365fef0aeafc78e8c
imadtg <i.haidi@outlook.com>
2025-07-01 14:39:45 +0100

    Set up a reverse proxy (because adblockers are blocking posthog)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/relay-oLEZ/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*", // TODO: move these to .env or interpolate using NEXT_PUBLIC_POSTHOG_HOST ?
+      },
+      {
+        source: "/relay-oLEZ/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+      {
+        source: "/relay-oLEZ/flags",
+        destination: "https://eu.i.posthog.com/flags",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
