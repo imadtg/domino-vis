c8b5963a730ccc7d6b0f5cbf76600313c2d08a80
imadtg <i.haidi@outlook.com>
2025-07-01 14:40:58 +0100

    Add missing comma

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -4,6 +4,6 @@ if(typeof process.env.NEXT_PUBLIC_POSTHOG_KEY == 'undefined'){
 }
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
   api_host: "/relay-oLEZ", // TODO: move to .env or interpolate from NEXT_PUBLIC_POSTHOG_HOST ?
-  ui_host: 'https://eu.posthog.com' 
+  ui_host: 'https://eu.posthog.com',
   defaults: "2025-05-24",
 });
