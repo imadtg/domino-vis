a77391a08861a1f25088bcd54bd503d13de647b3
imadtg <i.haidi@outlook.com>
2025-07-01 01:32:43 +0100

    Fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,15 +12,17 @@ export default function RootLayout({ children }: Props) {
     <StoreProvider>
       <html lang="en">
         <body>
-          <Script
-            strategy="beforeInteractive" // anchored positioning polyfill, since it isn't supported in firefox yet...
-          >
-            {`
+          {/* commented out because it causes hydration issues with instrumentation-client.ts, and it didnt even work in the first place...
+            <Script
+              strategy="beforeInteractive" // anchored positioning polyfill, since it isn't supported in firefox yet...
+            >
+              {`
             if (!("anchorName" in document.documentElement.style)) {
               import("https://unpkg.com/@oddbird/css-anchor-positioning");
-            }
-            `}
-          </Script>
+              }
+              `}
+            </Script>
+          */}
           <main className="flex flex-col items-center justify-center overflow-hidden">
             <MotionProvider>{children}</MotionProvider>
           </main>
