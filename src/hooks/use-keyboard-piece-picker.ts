9a25a061d153c3e9728b2087ba0f3f8c00463cfa
imadtg <i.haidi@outlook.com>
2025-09-13 14:12:54 +0100

    docs: add comments to explain how the keyboard piece picker hook logic works

--- src/hooks/use-keyboard-piece-picker.ts
+++ src/hooks/use-keyboard-piece-picker.ts
@@ -14,6 +14,15 @@ export default function useKeyboardPiecePicker({
   React.useEffect(() => {
     function handlePipKeyDown(event: KeyboardEvent) {
       if (!["0", "1", "2", "3", "4", "5", "6"].includes(event.key)) return;
+      // initially, first pip and second pip are undefined, on a pip keystroke,
+      // we move the old value of firstPip to secondPip and put the keystroke as firstPip
+      // and each time they constitute a piece we call onPick
+      // and flush below will reset them to undefined
+      // for example:
+      // firstPip.current === undefined, secondPip.current === undefined (no pip number clicked yet)
+      // firstPip.current === 3, secondPip.current === undefined (user clicked 3)
+      // firstPip.current === 6, secondPip.current === 3 (user clicked 6), onPick called with piece [6|3]
+      // firstPip.current === undefined, secondPip.current === undefined (consuming code calls flush (when a user selects the piece in the game init menu for example))
       if (typeof firstPip.current !== "undefined") {
         secondPip.current = firstPip.current;
         firstPip.current = parseInt(event.key);
