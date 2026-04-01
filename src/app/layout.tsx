7b7c6c696ad8feffc04165ae4851276553f69e34
imadtg <i.haidi@outlook.com>
2026-04-01 08:34:06 +0100

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,12 @@ import { StoreProvider } from "./StoreProvider";
 import "./globals.css";
 import * as React from "react";
 import { MotionProvider } from "./MotionProvider";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
 interface Props {
   readonly children: ReactNode;
 }
@@ -10,7 +16,7 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <StoreProvider>
-      <html lang="en">
+      <html lang="en" className={cn("font-sans", geist.variable)}>
         <body>
           {/* commented out because it causes hydration issues with instrumentation-client.ts, and it didnt even work in the first place...
             <Script
